9fb08c014401ce24cdb2a33e4c33fd9f26167f72
Lizbeth-girl <saezlizbethe6@gamail.com>
2024-11-05 09:44:13 -0500

    Windws

---
+++ src/main/java/ec/edu/espoch/windows/Windows.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package ec.edu.espoch.windows;
+
+/**
+ *
+ * @author PERSONAL
+ */
+public class Windows {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
